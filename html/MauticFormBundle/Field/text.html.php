181260f8b6b1bda03b4023119091f830eecf0ef8
Lukáš Pokorný <luk4s.pokorny@gmail.com>
2020-02-26 08:36:09 +0100

    v1.0.0

---
+++ html/MauticFormBundle/Field/text.html.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * @copyright   2014 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+$containerType     = (isset($type)) ? $type : 'text';
+$defaultInputClass = (isset($inputClass)) ? $inputClass : 'input';
+
+include __DIR__.'/field_helper.php';
+
+if ($required) {
+    $field['label'] = "{$field['label']} *";
+}
+$label = (!$field['showLabel']) ? '' : <<<HTML
+
+                <label $labelAttr>{$field['label']}</label>
+HTML;
+
+$help = (empty($field['helpMessage'])) ? '' : <<<HTML
+
+                <span class="mauticform-helpmessage">{$field['helpMessage']}</span>
+HTML;
+
+if ($containerType == 'textarea'):
+$textInput = <<<HTML
+
+                <textarea $inputAttr rows="4">{$field['defaultValue']}</textarea>
+HTML;
+
+else:
+$textInput = <<<HTML
+
+                <input {$inputAttr} type="$containerType"/>
+HTML;
+endif;
+
+$html = <<<HTML
+
+            <div $containerAttr class="input-group">
+            {$textInput}
+            {$label}
+            {$help}
+                <span class="mauticform-errormsg" style="display: none;">$validationMessage</span>
+            </div>
+
+HTML;
+
+echo $html;
---
+++ html/MauticFormBundle/Field/textarea.html.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * @copyright   2014 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+$type           = 'textarea';
+$inputClass     = 'textarea';
+$containerClass = 'text';
+include __DIR__.'/text.html.php';
